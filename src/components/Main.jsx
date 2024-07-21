7ea36446beab9af2deeba29e2ea392c72876730f
Rizwan Ashraf <rizwanashraf@rizwans-mini.lan>
2024-07-21 14:28:45 +0100

    mobile font size adjust

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,13 +8,13 @@ const Main = () => {
         <video src={videoBg} autoPlay loop muted />
         <div className="content">
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full text-center'>
-        <h1 className='text-4xl sm:text-9xl font-bold text-[#e0b92d]'>
+        <h1 className='text-7xl sm:text-9xl font-bold text-[#e0b92d]'>
           CALIPHATE
         </h1>
-        <p className='pt-2 text-4xl sm:text-5xl font-bold text-[#e0b92d]'>
+        <p className='pt-2 text-3xl sm:text-5xl font-bold text-[#e0b92d]'>
             LUXURY STREETWEAR
         </p>
-        <h1 className='pt-8 text-4xl sm:text-4xl font-bold text-[#8e1f1f]'>
+        <h1 className='pt-2 text-3xl sm:text-4xl font-bold text-[#8e1f1f]'>
           COMING SOON
         </h1>
 
