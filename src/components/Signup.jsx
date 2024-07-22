7366e08aeb0a8bfa3e745a2f221d0d5311eecc88
Rizwan Ashraf <rizwanashraf@rizwans-mini.lan>
2024-07-22 16:45:04 +0100

    signup form adjustment for mobile

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -42,16 +42,16 @@ export const Signup = () => {
 
             </div>
             <div className='pb-4 max-w-[1000px] w-full grid grid-cols-4 gap-8 px-2'>             
-              <p><a className='pt-2 flex sm:justify-center text-gray-300 hover:text-[#dbb434]'
+              <p><a className='pt-2 flex justify-center text-gray-300 hover:text-[#dbb434]'
               href='https://www.instagram.com/'><FaInstagram size={30} /></a></p>
-                          <p><a className='pt-2 flex sm:justify-center text-gray-300 hover:text-[#dbb434]'
+                          <p><a className='pt-2 flex justify-center text-gray-300 hover:text-[#dbb434]'
               href='https://www.facebook.com/'><CiFacebook size={30} /></a></p>
-                          <p><a className='pt-2 flex sm:justify-center text-gray-300 hover:text-[#dbb434]'
+                          <p><a className='pt-2 flex justify-center text-gray-300 hover:text-[#dbb434]'
               href='https://www.x.com/'><FaXTwitter size={30} /></a></p>
                           <p>        <a className='pt-2 flex sm:justify-center' href={pdf2} download="Caliphate Clo. Diversity & Inclusion Statement.pdf"><MdOutlineDiversity3 size={30} className='hover:text-[#dbb434]'/></a></p>
             </div>
           </div>
-            <input className='text-center my-4 mx-32 p-2 bg-gray-300' type="email" placeholder='Email address' name='email' />
+            <input className='text-center my-4 mx-10 p-2 bg-gray-300' type="email" placeholder='Email address' name='email' />
               <button className='text-white border-2 hover:bg-[#dbb434] hover:border-[#dbb434] hover:text-[#000000] px-8 py-3 my-2 mx-auto flex items-center' type='submit' value='Send'>Sign Up</button>
         </form>
     </div>
