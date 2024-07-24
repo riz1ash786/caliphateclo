ac105c3595288a5e656feaf2abf31987f33b6fb2
Rizwan Ashraf <rizwanashraf@rizwans-mini.lan>
2024-07-24 18:55:58 +0100

    fix spacing mob to desktop

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -31,17 +31,17 @@ export const Signup = () => {
   return (
     <div name='signup' className='w-full md:h-screen sm:h-auto bg-gradient-to-r from-[#000000] to-[#000000] flex justify-center items-center p-4'>
         <div>
-            <div className='pt-28 text-center'>
+            <div className='pt-28 sm:pt-0 py-10 text-center'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#dbb434] text-gray-300'>SIGN UP</p>
-                <p className='text-gray-300 py-8'>Stay connected for the latest drops and exclusive brand updates;</p>
+
             </div>
-            <div className='pb-4 max-w-[1000px] w-full grid gap-8 px-2 text-gray-300'>
+            <div className='pb-10 sm:pb-2 pt-0 sm:pt-4 max-w-[1000px] w-full grid gap-8 px-2 text-gray-300'>
             <div className='text-center items-center'>
               <p>Business Inquiries</p>
               <p>Email: <a className='hover:text-[#dbb434]' href='mailto:info@caliphateclo.com'>info@caliphateclo.com</a></p>
 
             </div>
-            <div className='pb-4 max-w-[1000px] w-full grid grid-cols-4 gap-8 px-2'>             
+            <div className='pt-16 max-w-[1000px] w-full grid grid-cols-4 gap-8 px-2'>             
               <p><a className='pt-2 flex justify-center text-gray-300 hover:text-[#dbb434]'
               href='https://www.instagram.com/'><FaInstagram size={30} /></a></p>
                           <p><a className='pt-2 flex justify-center text-gray-300 hover:text-[#dbb434]'
@@ -54,9 +54,9 @@ export const Signup = () => {
             {/* <input className='text-center my-4 mx-10 p-2 bg-gray-300' type="email" placeholder='Email address' name='email' />
               <button className='text-white border-2 hover:bg-[#dbb434] hover:border-[#dbb434] hover:text-[#000000] px-8 py-3 my-2 mx-auto flex items-center' type='submit' value='Send'>Sign Up</button> */}
               
-    <div className="mx-auto mt-4 max-w-7xl px-6 lg:px-8">
+    <div className="pt-8 sm:pt-4 mx-auto max-w-7xl px-6 lg:px-8 pb-20 sm:pb-0">
     <div
-        className="relative isolate overflow-hidden bg-[#121212] px-6 py-12 shadow-2xl rounded-2xl sm:rounded-3xl sm:px-24 xl:py-32">
+        className="relative isolate overflow-hidden bg-[#000000] px-6 py-12 shadow-2xl rounded-xl sm:rounded-2xl sm:px-24 xl:py-24">
        
         <h2 className="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-gray-300 sm:text-4xl">Keep Updated
         </h2>
@@ -70,7 +70,7 @@ export const Signup = () => {
             <label htmlFor="email-address" className="sr-only">Email address</label>
             <input id="email-address" name="email" type="email" autoComplete="email" required="" className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-gray-300 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-gray-300 sm:text-sm sm:leading-6" placeholder="Enter your email"/>
 
-            <button type="submit" className="flex-none rounded-md bg-gray-300 px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-[#dbb434] hover:text-[#000000] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-300">Notify me</button>
+            <button type="submit"className="flex-none rounded-md bg-gray-300 px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-[#dbb434] hover:text-[#000000] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-300">Notify me</button>
             </div>
         </form>
 
@@ -86,11 +86,12 @@ export const Signup = () => {
                 </radialGradient>
             </defs>
         </svg>
-
     </div>
 </div>
 </div>
 </div>
+
+
 
   )
   
